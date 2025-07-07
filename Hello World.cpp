e9a83915ec074079efb9759eb476b05dbeecb340
Jason Kraisser <jasonkraisser7@gmail.com>
2025-07-07 10:45:53 -0500

    GO!

---
+++ Hello World.cpp
@@ -0,0 +1,11 @@
+// Demo Project Jason Kraisser
+
+#include <iostream>
+
+void main()
+{
+	std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging Menu
